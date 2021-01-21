3cbe753e6289411695751d7ad678c6267821030c
Elys <elys.sanntos@gmail.com>
2021-01-20 22:36:11 -0300

    Listas
    Acessando um elemento especifico
    Alterando o item
    Listas iniciadas
    Listas vazias
    Operador Lambida

---
+++ C# Collections parte 1 Listas, arrays, listas ligadas, dicionários e conjuntos/Listas/Acesso1Elemento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> aulas = new List<string>();
+            aulas.Add("aulaIntro");
+            aulas.Add("aulaModelando");
+            aulas.Add("aulaSets");
+
+
+            
+
+            Console.WriteLine("A primeira aula é : \n - " + aulas[0]);
+            Console.WriteLine("A primeira aula é : \n - " + aulas.First());
+
+
+            Console.WriteLine("A última aula é : \n - " + aulas[aulas.Count - 1]);
+            Console.WriteLine("A última aula é : \n - " + aulas.Last());
+
+#if DEBUG
+            Console.WriteLine(" \r\n Para finalizar clique na tecla enter. \r\n");
+            Console.ReadLine();
+#endif
+        }
+
+
+
+    }
+
+    }
+
+
+
+
---
+++ C# Collections parte 1 Listas, arrays, listas ligadas, dicionários e conjuntos/Listas/Listas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introdução às Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            List<string> aulas = new List<string>
+    {
+        aulaIntro,
+        aulaModelando,
+        aulaSets
+    };
+            Imprimir(aulas);
+        }
+
+        private static void Imprimir(List<string> aulas)
+        {
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine("- " + aula);
+            }
+
+#if DEBUG
+            Console.WriteLine(" \r\n Para finalizar clique na tecla enter. \r\n");
+            Console.ReadLine();
+#endif
+        }
+
+    }
+
+       
+    }
+
